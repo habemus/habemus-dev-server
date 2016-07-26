d68dcc31c439e5aea2a4943812fe338bf9bc8f21
Simon <simonxd@gmail.com>
2016-07-25 22:00:59 -0300

    handling not found error

--- server/app/error-handlers/dev-server-html5.js
+++ server/app/error-handlers/dev-server-html5.js
@@ -1,3 +1,24 @@
+// own
+const errors = require('../../../shared/errors');
+
 module.exports = function (app, options) {
+  app.use(function (err, req, res, next) {
+
+    if (err instanceof errors.DevServerHTML5Error) {
+
+      switch (err.name) {
+        case 'NotFound':
+          res.status(404).end();
+          break;
+
+        default:
+          next(err);
+          break;
+      }
+
+    } else {
+      next(err);
+    }
+  });
 
 };
